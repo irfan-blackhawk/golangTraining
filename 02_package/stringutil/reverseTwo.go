a7442543b3b45b1bb560ccb1b714b81c0f5241fe
irfan-blackhawk <irfan.blackhawk@gmail.com>
2017-01-28 23:49:21 +0400

    golangTraining learning packages updates

--- 02_package/stringutil/reverseTwo.go
+++ 02_package/stringutil/reverseTwo.go
@@ -4,7 +4,7 @@ func reverseTwo(s string) string {
 	r := []rune(s)
 
 	for i, j := 0, len(r)-1; i < len(r)/2; i,j = i +1, j-1 {
-		r[i], r[i] = r[j], r[i]
+		r[i], r[j] = r[j], r[i]
 	}
 
 	return string(r)
