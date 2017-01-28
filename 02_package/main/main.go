97cb0037a05f1c4231f5d258332e9aa0f39b28c9
irfan-blackhawk <irfan.blackhawk@gmail.com>
2017-01-28 23:46:57 +0400

    golangTraining learning packages

---
+++ 02_package/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/udemyTraining/02_package/stringutil"
+	"github.com/GoesToEleven/udemyTraining/02_package/winniepooh"
+)
+
+func main()  {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
