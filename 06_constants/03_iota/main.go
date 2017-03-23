a7f30a78e9a4adf5779de71cfe32e7e4644c4fc7
irfan-blackhawk <irfan.blackhawk@gmail.com>
2017-03-23 09:09:30 +0400

    golangTraining iota doc

--- 06_constants/03_iota/main.go
+++ 06_constants/03_iota/main.go
@@ -8,6 +8,13 @@ const (
 	c = iota //3
 )
 
+/**
+* https://golang.org/ref/spec#Iota
+* Within a constant declaration, the predeclared identifier iota represents successive untyped integer constants.
+* It is reset to 0 whenever the reserved word const appears in the source and increments after each ConstSpec.
+* It can be used to construct a set of related constants:
+*/
+
 func main()  {
 	fmt.Println(a)
 	fmt.Println(b)
